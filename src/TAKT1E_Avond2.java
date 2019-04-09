c954b2e9357d0c2c1c1d48389d5cb60cee4b7851
Arnold Schulte <arnoldschulte@gmail.com>
2019-04-09 19:59:36 +0200

    Verbetering na bespreking opdracht

--- src/TAKT1E_Avond2.java
+++ src/TAKT1E_Avond2.java
@@ -4,42 +4,32 @@ public class TAKT1E_Avond2 {
 
     int[] geboortejaar = {1954, 1958, 1958, 1963, 1963, 1963, 1964, 1970, 1970, 1970};
     String[] geslacht = {"V", "M", "V", "m", "v", "V", "?", "v", "v", "m"};
-    int loopteller = 0;
-    int mannenPerJaar = 0;
-    int vrouwenPerJaar = 0;
-    int onbekendGeslacht = 0;
+
+    int loopTeller = 0;
     int totaalMannen = 0;
     int totaalVrouwen = 0;
 
-    while (loopteller < geboortejaar.length) {
-      System.out.println("Het geboortejaar is: " + geboortejaar[loopteller]);
-      int hulpjaar = geboortejaar[loopteller];
+    while (loopTeller < geboortejaar.length) {
+      System.out.println("Het geboortejaar is: " + geboortejaar[loopTeller]);
+      int hulpjaar = geboortejaar[loopTeller];
 
-      for (int i = 0; i < geboortejaar.length; i++) {
-        if (hulpjaar == geboortejaar[i]) {
-          if (geslacht[i].equalsIgnoreCase("M")) {
+      int mannenPerJaar = 0;
+      int vrouwenPerJaar = 0;
+
+      while (loopTeller < geboortejaar.length && geboortejaar[loopTeller] == hulpjaar) {
+        if (geslacht[loopTeller].equalsIgnoreCase("M")) {
             mannenPerJaar++;
             totaalMannen++;
-          } else if (geslacht[i].equalsIgnoreCase("V")) {
+        } else if (geslacht[loopTeller].equalsIgnoreCase("V")) {
             vrouwenPerJaar++;
             totaalVrouwen++;
           } else {
-            onbekendGeslacht++;
+          System.out.println("Onbekend geslacht: " + geboortejaar[loopTeller]);
           }
-          if (onbekendGeslacht > 0) {
-            System.out.println("Onbekend geslacht: ?");
-            onbekendGeslacht = 0;
-          }
-          loopteller++;
-
-        } else {
-//            Doe niks
+        loopTeller++;
         }
-      }
       System.out.println("Aantal mannen: " + mannenPerJaar);
       System.out.println("Aantal vrouwen: " + vrouwenPerJaar);
-      mannenPerJaar = 0;
-      vrouwenPerJaar = 0;
     }
     System.out.println("--- Totalen ---");
     System.out.println("Totaal aantal mannen: " + totaalMannen);
