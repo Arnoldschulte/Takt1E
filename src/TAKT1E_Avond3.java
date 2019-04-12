925c560ed49002c7186f94d75758c6eee22c1998
Arnold Schulte <arnoldschulte@gmail.com>
2019-04-12 23:45:39 +0200

    Opdracht 316 voor avond 3 uitgebreid

--- src/TAKT1E_Avond3.java
+++ src/TAKT1E_Avond3.java
@@ -1,6 +1,15 @@
 public class TAKT1E_Avond3 {
 
-  public static void main(String[] args) throws Exception {
+  public static final String VOORNAAM = "Voornaam";
+  public static final String VOORVOEGSEL = "Voorvoegsel";
+  public static final String ACHTERNAAM = "Achternaam";
+  public static final String GEBOORTEDATUM = "Geboortedatum";
+  public static final String GEBOORTEJAAR = "Geboortejaar";
+  public static final String GESLACHT = "Geslacht";
+  public static final String MAN = "M";
+  public static final String VROUW = "V";
+
+  public static void main(String[] args) {
 
     String[] persoon = new String[10];
     persoon[0] = "24-08-1954;V; Dijk, van;Maria";
@@ -14,20 +23,120 @@ public class TAKT1E_Avond3 {
     persoon[8] = "01-06-1970;v; Boogerts,den;Ester";
     persoon[9] = "19-09-1970;m; Konincks;Sven";
 
-    String test = leesVeld(persoon[0],"Geboortejaar");
-    System.out.println(test);
-    System.out.println(leesVeld(persoon[1],"Test"));
+    int totaalMannen = 0;
+    int totaalVrouwen = 0;
+    int totaalRest = 0;
+    int counter = 0;
+    int maximum = persoon.length;
+
+    printPersonenOverzicht(persoon);
+
+    while (counter < maximum) {
+      int geboortejaar = Integer.parseInt(leesVeld(persoon, counter, GEBOORTEJAAR));
+      System.out.println("Het geboortejaar is: " + geboortejaar);
+      int hulpjaar = geboortejaar;
+
+      int aantalMannen = 0;
+      int aantalVrouwen = 0;
+      int rest = 0;
+
+      while (counter < maximum && Integer.parseInt(leesVeld(persoon, counter, GEBOORTEJAAR)) == hulpjaar) {
+        switch (leesVeld(persoon, counter, GESLACHT)) {
+          case MAN:
+            aantalMannen++;
+            totaalMannen++;
+            break;
+          case VROUW:
+            aantalVrouwen++;
+            totaalVrouwen++;
+            break;
+          default:
+            rest++;
+            totaalRest++;
+            break;
+        }
+        counter++;
+      }
+      printRondeOverzicht(aantalMannen, aantalVrouwen, rest);
+    }
+    printTotaaloverzicht(totaalMannen, totaalVrouwen, totaalRest);
   }
 
-  public static String leesVeld(String persoon, String persoonsData) throws Exception {
-    String[] splitsRegel = persoon.split(";");
-    switch (persoonsData) {
-      case "Geboortejaar":
-        return splitsRegel[0].substring(6);
-      case "Geslacht":
-        return splitsRegel[1].toUpperCase();
-      default:
-        throw new Exception("Geen geldige persoonsdata gekozen");
+  private static void printPersonenOverzicht(String[] persoon) {
+    System.out.println("--- Personenoverzicht ---");
+    for (int i = 0; i < persoon.length; i++) {
+      String persoonsnummer = String.valueOf(i + 1);
+      String naam = aanhefKeuze(persoon, i) + " " + leesVeld(persoon, i, VOORNAAM) + " " + leesVeld(persoon, i, VOORVOEGSEL) + " " + leesVeld(persoon, i, ACHTERNAAM);
+
+      System.out.println("Persoon " + persoonsnummer + " :");
+      System.out.println(naam);
+      System.out.println("Geboren op: " + leesVeld(persoon, i, GEBOORTEDATUM));
+      System.out.println("---------------");
     }
+  }
+
+  public static String aanhefKeuze(String[] persoon, int i) {
+    String aanhef;
+    switch (leesVeld(persoon, i, GESLACHT)) {
+      case MAN:
+        return aanhef = "Dhr.";
+      case VROUW:
+        return aanhef = "Mevr.";
+      default:
+        return aanhef = "";
+    }
+  }
+
+  public static String leesVeld(String[] persoon, int index, String persoonsData) {
+    String[] splitsRegel = persoon[index].split(";", -1);
+    String[] achternaamVoorvoegsel = splitsRegel[2].split(",", -1);
+
+    String geboortedatum = splitsRegel[0];
+    String geboortejaar = splitsRegel[0].substring(6);
+    String geslacht = splitsRegel[1].toUpperCase();
+    String voornaam = splitsRegel[3];
+    String achternaam;
+    String voorvoegsel;
+
+    if (achternaamVoorvoegsel.length < 2) {
+      achternaam = achternaamVoorvoegsel[0].trim();
+      voorvoegsel = "";
+    } else {
+      achternaam = achternaamVoorvoegsel[0].trim();
+      voorvoegsel = achternaamVoorvoegsel[1].trim();
+    }
+
+    switch (persoonsData) {
+      case GEBOORTEDATUM:
+        return geboortedatum;
+      case GEBOORTEJAAR:
+        return geboortejaar;
+      case GESLACHT:
+        return geslacht;
+      case VOORNAAM:
+        return voornaam;
+      case VOORVOEGSEL:
+        return voorvoegsel;
+      case ACHTERNAAM:
+        return achternaam;
+      default:
+        throw new IllegalStateException("Geen geldige persoonsdata gekozen");
+    }
+    }
+
+  private static void printRondeOverzicht(int aantalMannen, int aantalVrouwen, int rest) {
+    System.out.println("Aantal mannen: " + aantalMannen);
+    System.out.println("Aantal vrouwen: " + aantalVrouwen);
+    System.out.println("Aantal onbekend: " + rest);
+  }
+
+  private static void printTotaaloverzicht(int totaalMannen, int totaalVrouwen, int totaalRest) {
+    int totaalPersonen = totaalMannen + totaalVrouwen + totaalRest;
+    System.out.println("--- Totalen ---");
+    System.out.println("Totaal aantal mannen: " + totaalMannen);
+    System.out.println("Totaal aantal vrouwen: " + totaalVrouwen);
+    System.out.println("Totaal aantal onbekend: " + totaalRest);
+    System.out.println("---------------");
+    System.out.println("Totaal aantal personen: " + totaalPersonen);
   }
 }
